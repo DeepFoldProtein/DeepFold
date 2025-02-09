a9ac39209f0cb892a08f8ed602e4bdf1b7393403
Minsoo Kim <vv137.xyz@gmail.com>
2025-02-09 18:09:11 +0900

    git feat(build): update setup.py for CUDA extension and GPU architecture support
    - Added dynamic GPU architecture detection for CUDA compilation.
    - Included support for multiple compute capabilities.
    - Updated extension name to "deepfold_C" and added CUTLASS path handling.
    - Improved source file management for CUDA extensions.

--- setup.py
+++ setup.py
@@ -1,87 +1,88 @@
 """Setup for pip package."""
 
-import importlib.util
+import os
+import sys
 
 import setuptools
+import torch
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-# import torch
-# from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+major = torch.cuda.get_device_properties(0).major
+minor = torch.cuda.get_device_properties(0).minor
+gpu_arch = f"{major}{minor}"
 
-spec = importlib.util.spec_from_file_location("package_info", "deepfold/package_info.py")
-package_info = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(package_info)
+compute_capabililties = [gpu_arch]
+# H100
+compute_capabililties.append("90")
+# L4, L40, RTX 4090
+compute_capabililties.append("89")
+# A40, A10, A16, A2, A6000, RTX 3090
+compute_capabililties.append("86")
+# A100, A30
+compute_capabililties.append("80")
 
-__description__ = package_info.__description__
-__license__ = package_info.__license__
-__package_name__ = package_info.__package_name__
-__version__ = package_info.__version__
+
+generator_flags = []
+cc_flag = []
+compute_capabililties = list(set(compute_capabililties))
+
+
+for cap in compute_capabililties:
+    cc_flag.append(f"-gencode=arch=compute_{cap},code=sm_{cap}")
+    cc_flag.append(f"-gencode=arch=compute_{cap},code=compute_{cap}")
+
+
+sources = [
+    "csrc/evoformer_attn/attention.cpp",
+    "csrc/evoformer_attn/attention_back.cu",
+    "csrc/evoformer_attn/attention_cu.cu",
+]
 
 
 ext_modules = []
-generator_flags = []
 
-# major = torch.cuda.get_device_properties(0).major
-# minor = torch.cuda.get_device_properties(0).minor
-# gpu_arch = f"{major}{minor}"
+cmdclass = {}
 
-# cc_flag = []
-# compute_capabililties = [gpu_arch]
+cutlass_path = os.environ.get("CUTLASS_PATH")
+if cutlass_path is None:
+    print("Please specify the CUTLASS repo directory as environment variable $CUTLASS_PATH", file=sys.stderr)
+else:
+    includes = [
+        f"{cutlass_path}/include",
+        f"{cutlass_path}/tools/util/include",
+    ]
+    includes = [os.path.abspath(p) for p in includes]
 
-# cc_flag = []
-# compute_capabililties = [gpu_arch]
-
-# for cap in compute_capabililties:
-#     cc_flag.append(f"-gencode=arch=compute_{cap},code=sm_{cap}")
-#     cc_flag.append(f"-gencode=arch=compute_{cap},code=compute_{cap}")
-
-
-# sources = [
-#     "csrc/evoformer_attn/attention.cpp",
-#     "csrc/evoformer_attn/attention_back.cu",
-#     "csrc/evoformer_attn/attention_cu.cu",
-# ]
-
-
-# ext_modules.append(
-#     CUDAExtension(
-#         name="deepfold_kernel",
-#         sources=sources,
-#         extra_compile_args={
-#             "cxx": ["-O2", "-std=c++17", *generator_flags],
-#             "nvcc": [
-#                 "-O2",
-#                 "-std=c++17",
-#                 "--use_fast_math",
-#                 f"-DGPU_ARCH={gpu_arch}",
-#                 *generator_flags,
-#                 *cc_flag,
-#             ],
-#         },
-#         include_dirs=[
-#             "csrc/cutlass/include",
-#         ],
-#         optional=True,
-#     )
-# )
+    ext_modules.append(
+        CUDAExtension(
+            name="deepfold_C",
+            sources=sources,
+            extra_compile_args={
+                "cxx": [
+                    "-O2",
+                    "-std=c++17",
+                    *generator_flags,
+                ],
+                "nvcc": [
+                    "-O2",
+                    "-std=c++17",
+                    "--use_fast_math",
+                    f"-DGPU_ARCH={gpu_arch}",
+                    "-allow-unsupported-compiler",
+                    *generator_flags,
+                    *cc_flag,
+                ],
+            },
+            include_dirs=includes,
+            optional=False,
+        )
+    )
+    cmdclass["build_ext"] = BuildExtension
 
 
 setuptools.setup(
-    name=__package_name__,
-    version=__version__,
-    description=__description__,
-    license=__license__,
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
-    ],
-    packages=setuptools.find_packages(exclude=["tests"]),
-    # install_requires=install_requires,
+    packages=setuptools.find_packages(include=["deepfold", "deepfold.*"]),
     include_package_data=True,
-    # ext_modules=ext_modules,
-    # cmdclass={"build_ext": BuildExtension},
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
 )
