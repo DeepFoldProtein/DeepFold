a19c49225d36f7fb01fc7b3ffb13d81b0fbcd8e3
Minsoo Kim <vv137.xyz@gmail.com>
2025-03-28 14:14:24 +0900

    clean: Remove unused modules and scripts

--- deepfold/runner/main.py
+++
@@ -1,4 +0,0 @@
-import deepfold.runner.interpreter as interpreter
-
-if __name__ == "__main__":
-    interpreter.main()
--- deepfold/runner/utils.py
+++
@@ -1,2 +0,0 @@
-def safe_filename(name: str) -> str:
-    return "".join([c if c.isalnum() or c in ("_", ".", "-") else "_" for c in name])
